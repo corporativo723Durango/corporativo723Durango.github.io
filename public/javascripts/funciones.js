2dba4f78fd767545128aabcac8fef773385f265d
corporativo723Durango <158262992+corporativo723Durango@users.noreply.github.com>
2024-01-31 07:50:26 -0600

    Update funciones.js
    cerrar login

--- public/javascripts/funciones.js
+++ public/javascripts/funciones.js
@@ -23,7 +23,7 @@ $(function(){
            .append($("<div>").addClass("login").html($("<input>").attr({"id":"usuario","type":"text","placeholder":"Usuario","required":"true"})))
             .append($("<div>").addClass("login").html($("<input>").attr({"id":"contra","type":"password","placeholder":"Contraseña","required":"true"})))
           )
-         alertify.confirm().set({title:"Inicio de Sesión",selector:'input[type="text"]',resizable:true,frameless:false,onok:function(cE){onAutenticar($("#usuario").val(),hex_md5($("#contra").val()),"form"); return false;},oncancel:function(){alertify.error("Cancelado");location.reload()}}).setContent($log[0]).resizeTo("50%",240).show()
+         alertify.confirm().set({title:"Inicio de Sesión",selector:'input[type="text"]',resizable:true,frameless:false,onok:function(cE){onAutenticar($("#usuario").val(),hex_md5($("#contra").val()),"form")},oncancel:function(){alertify.error("Cancelado");location.reload()}}).setContent($log[0]).resizeTo("50%",240).show()
          $(".ajs-dialog").addClass("fondoForms p35")
     }else{     
       s=JSON.parse(sesion) 
